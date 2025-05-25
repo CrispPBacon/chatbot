7e493bff4c545ca78b2c865061a7a00f98b8d182
Allan Soriano <al.sorianox@gmail.com>
2025-05-25 23:01:05 +0800

    UPDATE api baseURL to prodURL

--- client/src/api/api.tsx
+++ client/src/api/api.tsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const backendUrl = `${import.meta.env.VITE_BACKEND_HOST}:${
-  import.meta.env.VITE_BACKEND_PORT
-}`;
+// const backendUrl = `${import.meta.env.VITE_BACKEND_HOST}:${
+//   import.meta.env.VITE_BACKEND_PORT
+// }`;
 
+const prodURL = `${import.meta.env.VITE_BACKEND_HOST}`;
 const api = axios.create({
-  baseURL: backendUrl,
+  baseURL: prodURL,
 });
 
 export default api;

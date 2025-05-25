61803ea4d08d2319afaaee97794a93609fd9672f
Allan Soriano <al.sorianox@gmail.com>
2025-05-25 22:52:30 +0800

    SETUP for Github pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import { AuthProvider } from "./context/AuthProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
+    <Router basename="/chatbot">
       <AuthProvider>
         <App />
       </AuthProvider>
